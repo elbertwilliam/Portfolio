ee10c270400293d66f6046901f07ea2b18407b55
elbert12 <elbertwilliam12@gmail.com>
2023-08-01 11:13:04 +1000

    navbar link fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-        to="/"
+        to="https://elbertwilliam.github.io/Portfolio/"
         className="flex items-center gap-2"
         onClick={() => {
           setActive("");
